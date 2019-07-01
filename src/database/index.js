d00eb68644d658567e9c8049e21d6def2300762b
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-07-01 20:52:39 -0300

    Configurando MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -12,14 +13,24 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
+  // POSTGRES
   init() {
     this.connection = new Sequelize(databaseConfig);
 
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  // MongoDB
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
   }
 }
 
